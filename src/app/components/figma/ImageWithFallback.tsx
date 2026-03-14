4b62314b953c050c1c3a4b8b925d059c412a3a8d
PeterBaiochi <gabrielbaiochi@gmail.com>
2026-03-13 22:38:27 -0300

    Update ImageWithFallback.tsx

--- src/app/components/figma/ImageWithFallback.tsx
+++ src/app/components/figma/ImageWithFallback.tsx
@@ -16,7 +16,7 @@ export function ImageWithFallback(
 
   return didError ? (
     <div
-      className={`inline-block bg-gray-100 text-center align-middle ${className ?? ""}`}
+      className={`inline-block bg-[#1a1a1f] text-center align-middle ${className ?? ""}`}
       style={style}
     >
       <div className="flex items-center justify-center w-full h-full">
